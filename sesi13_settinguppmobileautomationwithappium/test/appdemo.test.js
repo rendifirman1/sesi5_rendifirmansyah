f73c31c4940af84fd0a9693225018b530cfa0efa
rendifirman1 <rendifirmansyah456@gmail.com>
2025-11-02 16:31:04 +0700

    Add Task Sesi 13 - Setting Up Mobile Automation with Appium

---
+++ sesi13_settinguppmobileautomationwithappium/test/appdemo.test.js
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+
+// Test Suite name
+describe('API Demo Testing', () => {
+
+  // Test Case name
+  it('should open the application successfully', async () => {
+    console.log('✅ The application has been opened successfully');
+  });
+
+});
